8b6d3595373274de49c0290f386d02de6c71d71e
skylark17 <id.agungpratama@gmail.com>
2020-10-24 14:41:56 +0700

    Hai Iam Joyeuse

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_joyeuse.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_joyeuse-user \
+    aosp_joyeuse-userdebug \
+    aosp_joyeuse-eng
